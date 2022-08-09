a7abc4ae139df87e0c5b6b4909ee4d1a149523f3
Lucie Caillat <lucie.caillat@gmail.com>
2022-08-09 17:13:22 +0200

    add the first 3 error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,38 @@ function closeModal() {
 };
 
 closeModalBtn.addEventListener("click", closeModal);
+
+//error message
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+
+function textErrorMessage(event, regex, nbr) {  
+  const inputTarget = document.querySelectorAll(".formData input")[nbr];
+  const errorMessage = document.querySelectorAll(".error-message")[nbr];  
+    if (regex.test(event.target.value)){
+      inputTarget.style.border = "none";
+    errorMessage.style.display = "none";
+  }else{
+    inputTarget.style.border = "2px solid #FF4E60";     
+    errorMessage.style.display = "block";   
+  }
+};
+
+first.addEventListener("change", function errorMessage (event){
+  textErrorMessage(event, /\w{2,}/, 0);
+});
+
+last.addEventListener("change", function errorMessage (event){
+  textErrorMessage(event, /\w{2,}/, 1);
+});
+
+email.addEventListener("change", function errorMessage (event){
+  textErrorMessage(event, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 2);
+});
+
+
+/*const errorMessage = document.createElement("p");  
+  document.querySelector(".formData").appendChild(errorMessage);
+  errorMessage.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  errorMessage.style.display = "none";*/
