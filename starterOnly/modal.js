4697e787b4cb171f35250d3357e8c6130b75558b
Lucie Caillat <lucie.caillat@gmail.com>
2022-08-12 17:20:08 +0200

    sending confirmation finished and add error message for number and terms of use

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,14 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBody = document.querySelector(".modal-body");
 const form = document.querySelector(".modal-body form");
-console.log(modalBody , form)
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelector(".close");
 const first = document.getElementById("first");
 const last = document.getElementById("last");
 const email = document.getElementById("email");
+const quantity = document.getElementById("quantity");
+const checkboxTermsOfUse = document.getElementById("checkbox1");
+const labelCheckboxTermsOfUse = document.querySelector(".checkbox2-label");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -52,6 +54,7 @@ function textErrorMessage(event, regex, index) {
 
 const regexAtLeastTwoLetters = /\w{2,}/;
 const regexEmailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regexNumberOnly = /^\d{1,2}$/;
 
 first.addEventListener("input", function errorMessage(event) {
   textErrorMessage(event, regexAtLeastTwoLetters, 0);
@@ -65,10 +68,35 @@ email.addEventListener("input", function errorMessage(event) {
   textErrorMessage(event, regexEmailFormat, 2);
 });
 
+quantity.addEventListener("input", function errorMessage(event) {
+  textErrorMessage(event, regexNumberOnly , 4);
+});
+
+checkboxTermsOfUse.addEventListener("input", function errorMessage(){
+  const errorMessage = document.querySelectorAll(".error-message")[5];
+  if (!checkboxTermsOfUse.checked){   
+    errorMessage.style.display = "block"; 
+    labelCheckboxTermsOfUse.style.color = "#FF4E60";
+  }else{
+    errorMessage.style.display = "none";
+    labelCheckboxTermsOfUse.style.color = "#fff"
+    
+  }; 
+})
+
 //sending confirmation
+const confirmationModal = document.createElement("div");
+confirmationModal.innerHTML = "<p class=\"confirmation-text\">Merci pour votre inscription </p> <button id=\"btn-close\"class=\"button btn-submit\">Fermer</button>";
+confirmationModal.classList.add("confirmation-modal")
 
 form.addEventListener("submit", function confirmation(event) {
-event.preventDefault();
-form.style.opacity = "0";
+  event.preventDefault();
+  form.style.opacity = "0";
+  modalBody.appendChild(confirmationModal);
+
+  //close modal with bottom btn
+  const closeConfirmationModalBtn = document.getElementById("btn-close");
+  closeConfirmationModalBtn.addEventListener("click", closeModal);
 });
+
 
